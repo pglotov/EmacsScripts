aff496fd1480084c0b963cff95a8dd296eb28d02
Takaaki ISHIKAWA <takaxp@ieee.org>
2012-01-12 13:21:54 +0900

    Keep cursor position of the previous column

--- SmoothScroll.el
+++ SmoothScroll.el
@@ -2,17 +2,23 @@
 ; use C-, to scroll up, C-. to scroll down
 
 ; author: Petr Glotov
+; modifiled: Takaaki Ishikawa
+
+(defvar smoothscroll-sticked-point (current-column))
+(run-with-idle-timer
+ 0.5 t '(lambda () (setq smoothscroll-sticked-point (current-column))))
 
 (defun scroll-one-down ()
   (interactive)
   (scroll-down 1)
   (forward-line -1)
-)
-(global-set-key [(control ?,)] 'scroll-one-down)
+  (move-to-column smoothscroll-sticked-point))
 
 (defun scroll-one-up ()
   (interactive)
   (scroll-up 1)
   (forward-line 1)
-)
+  (move-to-column smoothscroll-sticked-point))
+
+(global-set-key [(control ?,)] 'scroll-one-down)
 (global-set-key [(control ?.)] 'scroll-one-up)
